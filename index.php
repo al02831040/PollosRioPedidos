1b3e7b7c0408da8439d0f7c0d66b1ad1cb1ec5cb
Sergio C Lopez <al02831040@tecmilenio.mx>
2021-12-10 19:17:34 -0600

    Primer commit desarrollo pollos rio

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<title>Panaderia</title>
+<meta name="description" content="Panaderia, calidad y buen sabor.">
+<meta name="viewport" content="width=device-width">
+<!-- Bootstrap Stylesheet -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Animate -->
+<link rel="stylesheet" href="css/animate.min.css">
+<!-- Google Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!-- FontAwesome Icons -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<!-- Normailize Stylesheet -->
+<link rel="stylesheet" href="css/normalize.min.css">
+<!-- Main Styles -->
+<link rel="stylesheet" href="css/templatemo_style.css">
+<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 7]>
+<![endif]-->
+
+
+<div id="responsive-menu">
+<ul class="menu-holder">
+<li><a href="#"><i class="fa fa-home"></i>Home</a></li>
+<li><a href="#services"><i class="fa fa-cogs"></i>Productor</a></li>
+<li><a href="#contact"><i class="fa fa-envelope"></i>Ingreso</a></li>
+</ul>
+</div>
+
+<!-- HEADER -->
+<header class="site-header">
+<div class="container">
+<div class="row">
+<div class="menu-holder">
+<div class="col-md-3 col-sm-2 logo">
+<a href="#" title="templatemo 420 ocean">
+<img src="images/templatemo-logo.png" alt="templatemo 420 ocean">
+</a>
+</div>
+<div class="col-md-7 col-sm-8">
+<nav class="main-menu hidden-xs">
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#services">Productos</a></li>
+<li><a href="#contact">Ingreso</a></li>
+</ul>
+</nav>
+</div>
+<div class="col-md-2 col-sm-2 col-xs-12">
+<ul class="social-top">
+<li><a href="#"><i class="fa fa-facebook"></i><span class="hidden-xs">Facebook</span></a></li>
+<li><a href="#"><i class="fa fa-twitter"></i><span class="hidden-xs">Twitter</span></a></li>
+<li><a href="#"><i class="fa fa-instagram"></i><span class="hidden-xs">Instagram</span></a></li>
+</ul>
+</div>
+</div>
+
+
+<div class="text-right visible-xs">
+<a href="#" id="mobile_menu"><span class="fa fa-bars"></span></a>
+</div>
+</div>
+</div>
+</header> <!-- .site-header -->
+
+
+<!-- TOP CONTENT -->
+<div class="top-c">
+<div class="container">
+<div class="row">
+<div class="col-md-offset-1 col-lg-5 col-md-5 col-xs-offset-1 col-sm-5 col-xs-8 col-xs-offset-2">
+<div class="topc-img">
+<img src="images/slide1.png" alt="">
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+<h3 class="topc-title">Panaderia Delicia</h3>
+<ul>
+<li><i class="fa fa-check"></i>Productos de Calidad</li>
+<li><i class="fa fa-check"></i>Una tadicion familiar</li>
+<li><i class="fa fa-check"></i>Cocinamos con amor para alimentar tu alma</li>
+<li><i class="fa fa-check"></i>Calidad y buen sabor</li>
+<li><i class="fa fa-check"></i>Servicio a Domicilio</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+
+<!-- SERVICiIOS -->
+<div class="content-section" id="services">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<div class="col-md-5 col-xs-12 text-center">
+<img src="images/product-1.png" alt="">
+</div>                    
+<div class="product-content col-md-7 col-xs-12">
+<h3>Pan Tradicional</h3>
+<p></p>
+</div>
+</div> <!-- .col-md-6 -->
+<div class="col-md-6">
+<div class="product-content col-md-7 col-xs-12">
+<h3>Especialidades</h3>
+<p></p>
+<p></p>
+</div>
+<div class="col-md-5 col-xs-12 text-center">
+<img src="images/product-2.png" alt="">
+</div>
+</div> <!-- .col-md-6 -->
+</div>
+</div>
+</div> <!-- #products -->
+
+
+<!-- REGISTRO -->
+<div class="content-section" id="contact">
+<div class="container">
+<div class="row">
+<div class="col-md-12 text-center section-header">
+<h2 class="section-title">Login</h2>
+<p>Cocinamos con amor para alimentar tu Alma.</p>
+</div>
+</div>
+<div class="row">
+<div class="col-md-5 contact-info">
+<p>Para registrarte y adquirir una cuenta, por favor comunicate a los siguientes telefonos:</p>
+<ul>
+<li><i class="fa fa-phone"></i> 090-080-0980</li>
+<li><i class="fa fa-phone"></i> 090-080-0980</li>
+<!--<li><i class="fa fa-map-marker"></i> 360 Aenean Quis Semper, Maecenas 10450</li>
+<li><i class="fa fa-envelope"></i> <a href="mailto:info@company.com">info@company.com</a></li>-->
+</ul>
+</div> <!-- .col-md-5 -->
+<div class="col-md-7">
+<div class="row">
+
+<form class="contact-form" role="form" requiredname="form" method="post" action="#">
+<fieldset class="col-md-6">
+<input type="email" name="email" class="form-control" placeholder="Email" pattern="[A-Za-z0-9_-.@]{1,50}" >
+</fieldset>
+<fieldset class="col-md-6">
+<input type="password" name="password" class="form-control" placeholder="Password" pattern="[A-Za-z0-9]{1,50}">
+</fieldset>
+</BR>
+<fieldset class="col-md-12">
+<input type="submit" value="Ingresar" class="main-button">
+</fieldset>
+</form>
+</div> <!-- .row -->
+</div> <!-- .col-md-8 -->
+</div> <!-- .row -->
+</div> <!-- .container -->
+</div> <!-- #contact -->
+
+
+<!-- FOOTER -->
+<footer class="site-footer">
+<div class="container">
+<div class="row">
+<div class="col-md-12 text-right">
+<p>Copyright &copy; 2021 VNPP</p>
+</div> <!-- .col-md-12 -->
+</div> <!-- .row -->
+</div> <!-- .container -->
+</footer> <!-- .site-footer -->
+
+
+<script src="js/vendor/jquery-1.10.1.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/templatemo_custom.js"></script> 
+
+</body>
+</html>
